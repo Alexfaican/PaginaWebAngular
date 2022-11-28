4d86eb885bf970d738dc96aefce36ce4994b99ad
AlexFaican <lizer12f@gmail.com>
2022-11-28 08:42:44 -0500

    Actualizacion

--- src/components/componente1/comp1/comp1.component.ts
+++ src/components/componente1/comp1/comp1.component.ts
@@ -10,10 +10,27 @@ export class Comp1Component implements OnInit {
 
   constructor(private router: Router) { }
 
+  vector: string [] = [];
+  suma:number=0;
+  suma2:number=0;
   ngOnInit(): void {
+    this.total();
   }
  navegacion1(){
   this.router.navigate(['web2'])
+ }
+ total(){
+  let num1:number=0;
+  let num2:number=2;
+  this.suma=num1 + num2;
+  console.log(this.suma);
+  this.suma2=this.suma+5;
+  console.log(this.suma2);
+
+  this.total2(this.suma2);
+ }
+ total2(sum:number){
+  console.log(sum*2);
  }
  navegacion2(){
   this.router.navigate(['web3'])
