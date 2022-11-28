1558324fbef48d134da20bd590b110ad21d984bf
AlexFaican <lizer12f@gmail.com>
2022-11-28 09:06:13 -0500

    Actualizacion2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { Comp1Component } from 'src/components/componente1/comp1/comp1.component';
-import { Comp2Component } from 'src/components/componente2/comp2/comp2.component';
-import { Comp3Component } from 'src/components/componente3/comp3/comp3.component';
-import { Comp4Component } from 'src/components/componente4/comp4/comp4.component';
+import { Comp1Component } from 'src/app/components/componente1/comp1/comp1.component';
+import { Comp2Component } from 'src/app/components/componente2/comp2/comp2.component';
+import { Comp3Component } from 'src/app/components/componente3/comp3/comp3.component';
+import { Comp4Component } from 'src/app/components/componente4/comp4/comp4.component';
 
 
 const routes: Routes = [
